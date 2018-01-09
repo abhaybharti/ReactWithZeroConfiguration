fd349630715422177394fbc9080c6a04f6f9e3e0
Abhay Bharti <abhaybharti@users.noreply.github.com>
2018-01-09 06:30:09 +0530

    09-01

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+	  var helloWorld ="welcome to React via variable";
+    return (
+      <div className="App">
+        <h2>{helloWorld}</h2>
+		
+      </div>
+    );
+  }
+}
